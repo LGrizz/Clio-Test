842ab46624c4eef8997d9ad7e50ef4393c26d9e7
Kyle Langille <langille.kyle@gmail.com>
2014-06-02 16:22:43 -0600

    Add create note view controller

---
+++ Clio-Test/Clio-Test/CreateNoteViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateNoteViewController.h
+//  Clio-Test
+//
+//  Created by Kyle Langille on 2014-06-02.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateNoteViewController : UIViewController
+
+@end
